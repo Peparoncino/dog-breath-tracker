fc61a282ce56982624027e1ec6a8febc6c56d864
Peparoncino <jackofalltrades1008@gmail.com>
2026-08-09 15:54:52 +0900

    feat: ライト・ダークテーマの切り替え機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import RecordList from "./components/RecordList";
 import Stopwatch from "./components/Stopwatch";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <div>
+      <ThemeToggle />
       <Stopwatch />
       <RecordList />
     </div>
